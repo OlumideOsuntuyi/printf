66b7aa2240fea208fde87ce883d1406cfd02b420
OlumideOsuntuyi <olugeorge61@gmail.com>
2023-08-22 18:07:15 -0700

    fix

--- main.h
+++ main.h
@@ -42,48 +42,32 @@ int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
 int print_octal(va_list l, flags_t *f);
 
-/* converter */
-/**
- * convert - Converts a number to a string representation in a given base
- * @num: The input number
- * @base: The base for conversion
- * @lowercase: Flag indicating if hexa values should be lowercase
- *
- * Return: The result string
- */
+/* conv_case */
 char *convert(unsigned long int num, int base, int lowercase);
 
-/* _printf */
-/**
- * _printf - Produces output according to a format
- * @format: Format string containing the characters and the specifiers
- *
- * Description: This function formats and prints the output according to the
- * format string. It handles various conversion specifiers.
- * Return: Length of the formatted output string
- */
+/* printf */
 int _printf(const char *format, ...);
 
-/* get_print */
+/* handle_print */
 int (*get_print(char s))(va_list, flags_t *);
 
-/* get_flag */
+/* get_flag_func */
 int get_flag(char s, flags_t *f);
 
-/* print_alpha */
+/* char_string */
 int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
 
-/* write_funcs */
+/* putchar */
 int _putchar(char c);
 int _puts(char *str);
 
-/* print_custom */
+/* custom_operators */
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
 
-/* print_address */
+/* print_addr */
 int print_address(va_list l, flags_t *f);
 
 /* print_percent */
