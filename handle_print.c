aa2ba1d70a9d7a90639ee3628b7884c53edc5683
OlumideOsuntuyi <olugeorge61@gmail.com>
2023-08-22 17:51:47 -0700

    fix

--- handle_print.c
+++ handle_print.c
@@ -1,43 +1,41 @@
 #include "main.h"
 
 /**
- * get_print - selects the appropriate printing function based on the specifier
- * @s: the character that holds the conversion specifier
- *
- * Description: This function loops through the shuffled func_arr[] array to
- * find a match between the specifier passed to _printf and the characters
- * in the struct. Return: a pointer to the matching printing function
+ * get_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
  */
 int (*get_print(char s))(va_list, flags_t *)
 {
-    ph func_arr[] = {
-        {'c', print_char},
-        {'r', print_rev},
-        {'x', print_hex},
-        {'p', print_address},
-        {'X', print_hex_big},
-        {'d', print_int},
-        {'S', print_bigS},
-        {'s', print_string},
-        {'%', print_percent},
-        {'o', print_octal},
-        {'R', print_rot13},
-        {'i', print_int},
-        {'u', print_unsigned},
-        {'b', print_binary}
-    };
-    
-    int num_functions = 14;
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
 
-    register int i;
+	register int i;
 
-    for (i = 0; i < num_functions; i++)
-    {
-        if (func_arr[i].c == s)
-            return (func_arr[i].f);
-    }
-
-    return (NULL);
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
 }
 
 
